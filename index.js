93f636e7fa9f73b65fbe2ca2e95de9efcabd4ba3
Amutha Muhunthan <amuthamuhunthan@Amuthas-iMac.local>
2021-09-16 14:55:02 +1000

    post new data using json-parser request body for auto generate header content-type

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+//  jason-parser to access data to dd new notes in the request body in JSON format.
+app.use(express.json());
 
 let notes = [
   {
@@ -24,16 +26,56 @@ let notes = [
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
-
+// fetch notes
 app.get("/api/notes", (request, response) => {
   response.json(notes);
+});
+// add new data
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+  console.log(notes);
+  response.json(note);
 });
 
 // check for individual id to load from the url to code to filter
 app.get("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
   const note = notes.find((note) => note.id === id);
-  response.json(note);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+// deleting data
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
 });
 
 const PORT = 3001;
