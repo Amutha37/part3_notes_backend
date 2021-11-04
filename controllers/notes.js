198cee344e42674bfe78c8f7a155cdd9b7fdc245
Amutha <amuthamuhunthan@Amuthas-iMac.local>
2021-11-04 13:58:21 +1100

    Establish best file structure and separate the different responsibilities of the app into a separate modules.

---
+++ controllers/notes.js
@@ -0,0 +1,73 @@
+const notesRouter = require('express').Router()
+const Note = require('../models/note')
+
+notesRouter.get('/', (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes)
+  })
+})
+
+notesRouter.get('/:id', (request, response, next) => {
+  // with mongoDB
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
+})
+
+//  add or POST  new notes
+notesRouter.post('/', (request, response, next) => {
+  const body = request.body
+  // with mongoDB
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote)
+    })
+    //   .then((savedNote) => savedNote.toJSON())
+    //   .then((savedAndFormatedNote) => {
+    //     response.json(savedAndFormatedNote)
+    //   })
+    .catch((error) => next(error))
+})
+
+// update database
+notesRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote)
+    })
+    .catch((error) => next(error))
+})
+
+// deleting data
+notesRouter.delete('/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
+})
+
+module.exports = notesRouter
