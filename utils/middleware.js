f340a97ab0b5d5636a78ee17e46862e9c0c5d410
Amutha Muhunthan <tamamutha@gmail.com>
2022-01-07 13:39:38 +1100

    testing user add and note

--- utils/middleware.js
+++ utils/middleware.js
@@ -24,7 +24,7 @@ const errorHandler = (error, request, response, next) => {
     return response.status(401).json({
       error: 'invalid token',
     })
-  } else if (error.name === 'TokenExpiredError') {
+  } else if (error.name === 'Inavalid Token') {
     return response.status(401).json({
       error: 'token expired',
     })
